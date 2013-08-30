77a018df6541c051c3519fae44d9e66f46a7f52d
Grarak <williye97@gmail.com>
2013-08-30 16:20:05 +0200

    add tuna support
    Signed-off-by: Grarak <williye97@gmail.com>

--- RomSwitcher/StartActivity.java
+++ RomSwitcher/StartActivity.java
@@ -7,44 +7,56 @@ import android.os.Bundle;
 public class StartActivity extends Activity {
 	
 	private static String device = android.os.Build.DEVICE.toString();
+	private static String board = android.os.Build.BOARD.toString();
+	public static String downloadlink;
+	public static String first;
+	public static String second;
+	public static String bootpartition;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		
 		//i9500
 		if (device.equals("ja3g")) {
-			MainActivity.downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/i9500";
-			MainActivity.first = "Samsung TouchWiz 4.2.2";
-			MainActivity.second = "Android Open Source Project 4.3";
-			MainActivity.bootpartition = "/dev/block/mmcblk0p9";
+			downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/i9500";
+			first = "Samsung TouchWiz 4.2.2";
+			second = "Android Open Source Project 4.3";
+			bootpartition = "/dev/block/mmcblk0p9";
 		}
 		
 		//Manta
 		if (device.equals("manta")) {
-			MainActivity.downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/manta";
-			MainActivity.first = "First Rom";
-			MainActivity.second = "Second Rom";
-			MainActivity.bootpartition = "/dev/block/platform/dw_mmc.0/by-name/boot";
+			downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/manta";
+			first = "First Rom";
+			second = "Second Rom";
+			bootpartition = "/dev/block/platform/dw_mmc.0/by-name/boot";
 		}
 		
 		//Odin
 		if (device.equals("odin") || device.equals("C6503")) {
-			MainActivity.downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/odin";
-			MainActivity.first = "Sony Timescape 4.2.2";
-			MainActivity.second = "Android Open Source Project 4.3";
-			MainActivity.bootpartition = "/dev/block/platform/msm_sdcc.1/by-name/boot";
+			downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/odin";
+			first = "Sony Timescape 4.2.2";
+			second = "Android Open Source Project 4.3";
+			bootpartition = "/dev/block/platform/msm_sdcc.1/by-name/boot";
+		}
+		
+		//Tuna
+		if (board.equals("tuna")) {
+			downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/tuna";
+			first = "First Rom";
+			second = "Second Rom";
+			bootpartition = "/dev/block/platform/omap/omap_hsmmc.0/by-name/boot";
 		}
         
 		//Yuga
 		if (device.equals("yuga") || device.equals("C6603")) {
-			MainActivity.downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/yuga";
-			MainActivity.first = "Sony Timescape 4.2.2";
-			MainActivity.second = "Android Open Source Project 4.3";
-			MainActivity.bootpartition = "/dev/block/platform/msm_sdcc.1/by-name/boot";
+			downloadlink = "https://raw.github.com/RomSwitchers/Downloadlinks/master/yuga";
+			first = "Sony Timescape 4.2.2";
+			second = "Android Open Source Project 4.3";
+			bootpartition = "/dev/block/platform/msm_sdcc.1/by-name/boot";
 		}
-        
+		
 		Intent i = new Intent(this, MainActivity.class);
 		startActivity(i);
 		finish();
